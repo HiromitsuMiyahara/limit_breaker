1f9b49cb14b0c02fce5ec3c741a7ebe3e2220138
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-06-15 06:06:13 +0000

    variant修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,16 @@ class Post < ApplicationRecord
   validates :body, presence: true
 
   #画像が設定されない場合はapp/assets/imagesに格納されているno_image.jpgという画像をデフォルト画像で表示する。
-  def get_image
+  def get_post_image
     unless post_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    post_image.variant(resize_to_limit: [100, 100]).processed
+    if post_image.variable?
+      post_image.variant(resize_to_limit: [100, 100]).processed
+    else
+      # デフォルト画像を返すか、元の画像をそのまま返す
+      post_image
+    end
   end
 end
