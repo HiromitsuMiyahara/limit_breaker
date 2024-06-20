4b816f0f9c0c894dc6cf7b38172a7a0605356254
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-06-20 06:18:49 +0000

    seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(
-  email: ENV['SECRET_EMAIL'],
-  password: ENV['SECRET_PASSWORD']
-)
+puts "seedの実行を開始"
+
+  Admin = User.find_or_create_by!(email: ENV['SECRET_EMAIL']) do |admin|
+    admin.name = "管理者"
+    admin.password = ENV['SECRET_PASSWORD']
+   end
+
+puts "seedの実行が完了しました"
