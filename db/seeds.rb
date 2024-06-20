2f4e8c09ab24435228fe7221309cd5508edf264a
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-06-20 07:35:41 +0000

    seed編集3

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,14 @@
 
 puts "seedの実行を開始"
 
-  Admin.find_or_create_by!(email: ENV['SECRET_EMAIL']) do |admin|
-    admin.password = ENV['SECRET_PASSWORD']
-   end
+User.find_or_create_by!(email: "arnold@example.com") do |user|
+  user.name = "Arnold"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+Admin.find_or_create_by!(email: ENV['SECRET_EMAIL']) do |admin|
+  admin.password = ENV['SECRET_PASSWORD']
+end
 
 puts "seedの実行が完了しました"
