09f46a69c4eb54469b9efe309985bd01a3032cb8
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-07-19 11:38:42 +0000

    いいね非同期化未完成

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,26 +3,23 @@ class Public::FavoritesController < ApplicationController
   before_action :find_favoritable
 
   def create
-    favorite = @favoritable.favorites.new(user: current_user)
-    if favorite.save
-      redirect_to request.referer
-    else
-      redirect_to request.referer, alert: "いいねに失敗しました。"
-    end
+    @favorite = @favoritable.favorites.new(user: current_user)
+    @favorite.save
+    # app/views/favorites/create.js.erbを参照する
   end
 
   def destroy
-    favorite = @favoritable.favorites.find_by(user: current_user)
-    if favorite.destroy
-      redirect_to request.referer
-    else
-      redirect_to request.referer, alert: "いいねの取り消しに失敗しました。"
-    end
+    @favorite = @favoritable.favorites.find_by(user: current_user)
+    @favorite.destroy
+    # app/views/favorites/destroy.js.erbを参照する
   end
 
   private
+  # ポリモーフィック関連の対象を見つける
+  # 適切なモデルのインスタンスを取得、取得したインスタンスは @favoritable というインスタンス変数に格納
     def find_favoritable
       params.each do |name, value|
+        # name が「xxx_id」の形式であるかをチェック
         if name =~ /(.+)_id$/
           @favoritable = $1.classify.constantize.find(value)
         end
