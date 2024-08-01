f9686888b520ac592b6ea88ce5bd41d6c2517390
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-08-01 14:56:46 +0000

    コメント非同期化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -7,12 +7,11 @@ class Public::PostCommentsController < ApplicationController
     @post_comment.user_id = current_user.id
     @post_comment.post_id = @post.id
     if @post_comment.save
-      flash[:notice] = "コメントを投稿しました。"
-      redirect_to request.referer
+      @post_comments = @post.post_comments.order(created_at: :desc)
+      # redirect_to request.referer
     else
       @post_comments = @post.post_comments.order(created_at: :desc)
       @user = @post.user
-      flash[:notice] = "コメントできませんでした"
       redirect_to post_path(@post)
     end
   end
@@ -20,11 +19,11 @@ class Public::PostCommentsController < ApplicationController
   def destroy
     @post_comment = PostComment.find(params[:id])
     if @post_comment.user == current_user
+      @post = @post_comment.post
       @post_comment.destroy
-      flash[:notice] = "コメントを削除しました。"
-      redirect_to request.referer
+      @post_comments = @post.post_comments.order(created_at: :desc)
+      # redirect_to request.referer
     else
-      flash[:notice] = "コメントの削除に失敗しました。"
       redirect_to request.referer
     end
   end
