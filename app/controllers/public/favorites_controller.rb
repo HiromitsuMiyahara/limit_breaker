625aa2ac8de07d58eccd73f1c161530be9b8aea2
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-08-02 12:42:51 +0000

    いいねコントローラーエラー修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -18,16 +18,10 @@ class Public::FavoritesController < ApplicationController
   # ポリモーフィック関連の対象を見つける
   # 適切なモデルのインスタンスを取得、取得したインスタンスは @favoritable というインスタンス変数に格納
     def find_favoritable
-      params.each do |name, value|
-        # name が「xxx_id」の形式であるかをチェック
-        if name =~ /(.+)_id$/
-          @favoritable = $1.classify.constantize.find(value)
-        end
-        # /(.+)_id$/ は、文字列の末尾が「_id」で終わるものを探している
-        # $1 には、「_id」の前の部分が入る（例えば、「post」や「post_comment」）。
-        # $1.classify は、この部分をクラス名（大文字始まり）に変換（例えば、「Post」や「PostComment」）。
-        # .constantize は、文字列を実際のクラスに変換
-        # .find(value) は、そのクラスの中で特定のIDを持つレコードを探す。
+      if params[:post_comment_id]
+        @favoritable = PostComment.find(params[:post_comment_id])
+      elsif params[:post_id]
+        @favoritable = Post.find(params[:post_id])
       end
     end
 end
